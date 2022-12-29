0cf1a898b7b00c6e37abd72d6893c8b588a3820a
Zach Hoeken <hoeken@gmail.com>
2022-12-29 20:15:21 +0000

    yay! logging is working.

--- log-signalk-over-ip.py
+++ log-signalk-over-ip.py
@@ -23,6 +23,7 @@ import argparse
 import os
 from pprint import pprint
 import websocket
+import math
 
 def on_message(wsapp, message):
 	print(message)
@@ -30,26 +31,30 @@ def on_message(wsapp, message):
 def main():
     
 	#these are our command line arguments
-	parser = argparse.ArgumentParser(description='Log NMEA0183 data from a server to a text file.')
+	parser = argparse.ArgumentParser(description='Log SignalK data from a server to a text file.')
 	parser.add_argument('-i', '--ip', action='store', default='192.168.50.20', help='IP Address of the NMEA0183 server')
 	parser.add_argument('-p', '--port', action='store', default='10110', help='Port of the NMEA0183 server')
 	parser.add_argument('-c', '--config', action='store', default='default', help='Sail configuration.  Can be any arbitrary text.  eg. "jib and main", "spinnaker", etc.')
     
 	args = parser.parse_args()
+	
+	#generate our filename
+	d = datetime.datetime.today()
+	now = d.strftime("%Y-%m-%d %H%M")
+	fname = "data/{}/signalk-log-{}-{}.txt".format(args.config, args.config, now)
 
-	#TWA - environment.wind.angleTrueWater
-	#TWS - environment.wind.speedTrue
-	#SOG - navigation.speedOverGround
-	#BSP - navigation.speedThroughWater
-	
+	#does our directory exist?
+	if not os.path.exists("data"):
+		os.mkdir("data")
+	if not os.path.exists("data/{}".format(args.config)):
+		os.mkdir("data/{}".format(args.config))
+
 	#connect to our signalk server
-	#wsapp = websocket.WebSocket("ws://boatpi.local/signalk/v1/stream?subscribe=none", on_message=on_message)
-	#wsapp.send(request)
-	#wsapp.run_forever()
-	
 	ws = websocket.WebSocket()
 	ws.connect("ws://boatpi.local/signalk/v1/stream?subscribe=none")
+	server_data = json.loads(ws.recv())
 
+	#we only want to get a few data points
 	request = """
 		{
 			"context": "vessels.self",
@@ -87,64 +92,53 @@ def main():
 	"""
 	ws.send(request)
 
-	while(True):
-		print(json.loads(ws.recv()))
-	
-	return True
-	
-	#generate our filename
-	d = datetime.datetime.today()
-	now = d.strftime("%Y-%m-%d %H%M")
-	fname = "data/{}/nmea0183-log-{}-{}.txt".format(args.config, args.config, now)
-
-	#does our directory exist?
-	if not os.path.exists("data/{}".format(args.config)):
-		os.mkdir("data/{}".format(args.config))
-
-	#init our data
-	line = ''
-	nmea = nmea0183.nmea0183()
-
-	#start our log file
 	with open(fname, 'w') as f:
 		try:
 			#log forever
 			while True:
+			
+				line = ws.recv()
+				output = json.loads(line)
+
+				#pprint(output)
+			
+				#write our line to our file...
+				jdata = {}
+				jdata['time'] = datetime.datetime.now().isoformat()
+				jdata['unix_time'] = time.time()
+				jdata['timezone'] = str(tzlocal.get_localzone())
+				jdata['source'] = 'signalk'
+				jdata['lines'] = line
+				f.write(json.dumps(jdata) + "\n")
+			
+				path = output['updates'][0]['values'][0]['path']
+				value = float(output['updates'][0]['values'][0]['value'])
+
+				#is it a known code?
+				#TWA - environment.wind.angleTrueWater
+				#TWS - environment.wind.speedTrue
+				#SOG - navigation.speedOverGround
+				#BSP - navigation.speedThroughWater
 				
-				#get data from the receiver
-				line += s.recv(1)
-				
-				#did we get a new line?
-				if line[-2:] == '\r\n':
-					
-					#is it a valid nmea0183 line?
-					output = nmea.parseline(line)
-					if nmea.valid:
-						
-						#write our line to our file...
-						jdata = {}
-						jdata['time'] = datetime.datetime.now().isoformat()
-						jdata['unix_time'] = time.time()
-						jdata['timezone'] = str(tzlocal.get_localzone())
-						jdata['lines'] = [line[0:-2]]
-						f.write(json.dumps(jdata) + "\n")
-						
-						#is it a known code?
-						if output.has_key('sog') and nmea.sentence == 'RMC':
-							print('[{}] SOG: {}'.format(nmea.sentence, output['sog']))
-						if output.has_key('water_speed') and nmea.sentence == 'VHW':
-							print('[{}] BSP: {}'.format(nmea.sentence, output['water_speed']))
-						if output.has_key('twa'):
-							print('[{}] TWA: {} TWS: {}'.format(nmea.sentence, output['twa'], output['tws']))
-					
-					#reset our buffer
-					line = ''
-				
+				if path == 'navigation.speedOverGround':
+					sog = round(value * 1.94384, 1)
+					print('SOG: {}'.format(sog))
+				if path == 'navigation.speedThroughWater':
+					bsp = round(value * 1.94384, 1)
+					print('BSP: {}'.format(bsp))
+				if path == 'environment.wind.speedTrue':
+					tws = round(value * 1.94384, 1)
+					print('TWS: {}'.format(tws))
+				if path == 'environment.wind.angleTrueWater':
+					twa = round(math.degrees(value))
+					print('TWA: {}'.format(twa))
+		except KeyboardInterrupt:
+			print("Exiting.")
 		except Exception as e:
 			pprint(e)
 
 	#clean up our socket
-	s.close()
+	ws.close()
 		
 if __name__ == '__main__':
 	main()
